08c4f0118e8432a6bf82a38a3bf350116db212bd
Aditya Gupta <mr.adityag123@gmail.com>
2024-03-10 00:47:03 +0530

    Day Wise upload

---
+++ Daywise/march01.js
@@ -0,0 +1,99 @@
+// const pi=Math.PI;
+// console.log(pi);
+// console.log(Math.round(pi));
+// console.log(Math.ceil(pi));
+// console.log(Math.floor(pi));
+
+// let num=5.8;
+// console.log(Math.round(num));
+// console.log(Math.random());
+
+// console.log(Math.floor(11*Math.random()));          //11 is exclusive
+// console.log(Math.floor(1000000*Math.random()));
+
+
+//String
+
+// enter \n(new line me chale gaye) have memory(eg. press to enter)   and shift+enter \(bas nayi line me dikha rhe hai) no memory 
+
+// const paragraph = "My name is aditya \
+// age 20";
+// console.log(paragraph);
+
+// Template
+// let a=5;
+// let b=6;
+// console.log(`hello ji ${a} kdnd ${b}`);
+// console.log("hello ji "+a+ " kdnd " +b);
+
+
+// let string = 'The JavaScript'
+// console.log(string.split())     // Changes to an array -> ["The JavaScript"]
+// console.log(string.split(' '))  // Split to an array at space -> ["The", "JavaScript"]
+
+// let str = '  The     JavaScript    '
+// console.log(str.trim())     // Changes to an array -> ["The JavaScript"]
+//console.log(string.split(' ')) 
+
+// let country = 'Finland'
+// console.log(country.includes('fin'))     // false
+// console.log(country.includes('Fin')) 
+
+
+//string.replace(oldsubstring, newsubstring)                 //doubt
+// let country = 'Finland'
+// let country1='Canada'
+// console.log(country.replace('Fin', 'Noman'))       // Nomanland
+// string.replace(country,country1)             //doubt
+
+
+// ARRAY>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+//[[],[],[]]   2d array
+
+let arr1=[1,2,3,4,5]
+
+// let arr2=Array(); //not used  array constructure
+let arr3="Java  script"
+// console.log(arr1);
+// console.log(arr1[4]);
+// console.log(arr3[4]);
+console.log(arr3.split(" "))
+// console.log(arr3.split("s"))
+
+// arr1[3]=14
+// console.log(arr1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
