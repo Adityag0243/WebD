ddb1af674bb79c56a97351332b7e350efe4a9600
Adityag0243 <mr.adityag123@gmail.com>
2024-05-13 12:51:42 +0530

    class js learning

---
+++ sem2/js/script.js
@@ -0,0 +1,33 @@
+// var a=10;
+// var b=12;
+// function hellooo(){
+//     var c=3;
+//     return c;
+// }
+// console.log(hello())
+// console.log(a)
+
+
+
+
+
+
+
+console.log(nam)
+var nam="IOI"
+
+sum();
+function sum(){
+    console.log("hello")
+}
+
+
+
+
+// console.log(name)
+// let name="IOI"
+
+// sum();
+// function sum(){
+//     console.log("hello")
+// }
---
+++ sem2/js/variable_datatype.js
@@ -0,0 +1,64 @@
+// variable
+
+var name1="IOI";
+let name2="IOI2";
+const name3="IOI3";   //const me usi line me value denge 
+console.table([name1,name2,name3])
+
+// Data type--> primitive and non primitive
+// primitive--> number ,string , bool , undef , null
+// undefined --> value is not assigned 
+// NULL : No value
+let loan_num=null;
+console.log(loan_num)
+loan_num=3;
+console.log(loan_num)
+
+//non-primitive array and object(array [] object {} , key is inedx in array , key is of own choice in obj)
+
+
+
+//  operator
+// assignment , arithmatic , logical && || ,relational
+// bitwise
+
+
+
+// conditions
+if(loan_num){
+    console.log("ok");
+}
+
+
+
+// switch
+let a=1
+switch(a){
+    case 1:
+        console.log(a)
+        break;
+    case 2:
+        console.log(a)
+        // break;
+    case 3:
+        console.log(a)
+        break;  
+    default:
+        break;
+}
+
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
