de500cfda22d2a6308ecf9ee9096910de0ed9fe5
Adityag0243 <mr.adityag123@gmail.com>
2024-06-01 07:11:48 +0530

    Dom and destructuring

---
+++ sem2/js/Dom/script.js
@@ -0,0 +1,13 @@
+// const var1=document.getElementsByTagName("h1"); //class and id both
+// console.log(var1)
+
+// const var1 = document.getElementsByID("first-id");
+// console.log(var2);
+
+
+// // // var
+
+// var2.outerText="Namaste";
+
+const var3=document.querySelector("#third-id");
+var3.outerHTML="okkkkkkkkkkkk";
--- sem2/js/destructuring , spread , rest/spread_and_rest.js
+++ sem2/js/destructuring , spread , rest/spread_and_rest.js
@@ -3,9 +3,7 @@
 
 
 const num=[1,2,3,4,5]
-// let ...num2=num;    //wrong
 
-//let num2=...num    //wrong
 
 let num2=[...num]
 // this will work for three arguments
@@ -15,11 +13,35 @@ function summation1(){
 console.log(summation1(1,2,3,4));
 
 
-// but it should work for dynammic input , BUT NOT WORKING
+
 function summation(){
-    let val=arguments.reduce((acc,curn)=> acc+curn);
+    let arr=Object.values(arguments); //as arguments is an object it must be converted in array
+    let val=arr.reduce((acc,curn)=> acc+curn);
     return val;
 }
-console.log(summation(1,2,3,4));
+console.log(summation(1,2,3,4,7,3));
+
+
+function summation2(...ars){
+    return ars.reduce((acc,curn)=> acc+curn)
+}
+console.log(summation2(1,2,3,4,5,6,7))
+
+
+
+
+
+function multisum(num1,num2,...args){
+    let arr=[];
+    arr[0]=num1*num2;
+    arr[1]=args.reduce((acc,curn)=> acc+curn,0)
+    // return num1*num2+args.reduce((acc,curn)=> acc+curn,0)
+    return arr
+}
+console.log(multisum(2,3,1,2,3,4,5))
+
+
+
+
 
 
