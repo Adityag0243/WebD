ff5eb714ced47a10f9da491a1f05156e1ce0db2a
Adityag0243 <mr.adityag123@gmail.com>
2024-06-03 11:59:43 +0530

    dom -> createContent eventing

--- sem2/js/Dom/script.js
+++ sem2/js/Dom/script.js
@@ -9,5 +9,22 @@
 
 // var2.outerText="Namaste";
 
-const var3=document.querySelector("#third-id");
-var3.outerHTML="okkkkkkkkkkkk";
+// const var3=document.querySelector("#third-id");
+// var3.outerHTML="okkkkkkkkkkkk";
+
+const target=document.querySelectorAll("h1");
+console.log(target)
+target[3].className="fourth-h1";
+target[3].id="fourth-id";
+
+target[3].setAttribute("class" , "fourth-hi2");
+
+target[3].textContent="text inserted by text content";
+
+target[2].innerHTML="not correct way to insert content";    
+// innerHTML se pura pura html element ke sath insert kr skyte hai
+
+target[2].innerHTML= "<p>not correct way to insert content</p>" ;
+
+
+
